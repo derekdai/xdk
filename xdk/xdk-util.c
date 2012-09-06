8d05da687ce170779158a81b286edcacb0809b54
Derek Dai <derek@alright.com.tw>
2012-09-06 19:56:15 +0800

    XdkUtil: enhance event dumping mechanism

--- xdk/xdk-util.c
+++ xdk/xdk-util.c
@@ -1,70 +1,309 @@
 #include "xdk-util.h"
 
-static const char * event_names[] = {
-	"Unknown",
-	"Unknown",
-	"XDK_EVENT_KEY_PRESS (KeyPress)",
-	"XDK_EVENT_KEY_RELEASE (KeyRelease)",
-	"XDK_EVENT_BUTTON_PRESS (ButtonPress)",
-	"XDK_EVENT_BUTTON_RELEASE (ButtonRelease)",
-	"XDK_EVENT_MOTION (MotionNotify)",
-	"XDK_EVENT_ENTER (EnterNotify)",
-	"XDK_EVENT_LEAVE (LeaveNotify)",
-	"XDK_EVENT_FOCUS_IN (FocusIn)",
-	"XDK_EVENT_FOCUS_OUT (FocusOut)",
-	"XDK_EVENT_KEYMAP (KeymapNotify)",
-	"XDK_EVENT_EXPOSE (Expose)",
-	"XDK_EVENT_GRAPHICS_EXPOSE (GraphicsExpose)",
-	"XDK_EVENT_NO_EXPOSE (NoExpose)",
-	"XDK_EVENT_VISIBILITY (VisibilityNotify)",
-	"XDK_EVENT_CREATE (CreateNotify)",
-	"XDK_EVENT_DESTROY (DestroyNotify)",
-	"XDK_EVENT_UNMAP (UnmapNotify)",
-	"XDK_EVENT_MAP (MapNotify)",
-	"XDK_EVENT_MAP_REQUEST (MapRequest)",
-	"XDK_EVENT_REPARENT (ReparentNotify)",
-	"XDK_EVENT_CONFIGURE (ConfigureNotify)",
-	"XDK_EVENT_CONFIGURE_REQUEST (ConfigureRequest)",
-	"XDK_EVENT_GRAVITY (GravityNotify)",
-	"XDK_EVENT_RESIZE_REQUEST (ResizeRequest)",
-	"XDK_EVENT_CIRCULATE (CirculateNotify)",
-	"XDK_EVENT_CIRCULATE_REQUEST = CirculateRequest)",
-	"XDK_EVENT_PROPERTY (PropertyNotify)",
-	"XDK_EVENT_SELECTION_CLEAR (SelectionClear)",
-	"XDK_EVENT_SELECTION_REQUEST (SelectionRequest)",
-	"XDK_EVENT_SELECTION (SelectionNotify)",
-	"XDK_EVENT_COLORMAP (ColormapNotify)",
-	"XDK_EVENT_CLIENT_MESSAGE (ClientMessage)",
-	"XDK_EVENT_MAPPING (MappingNotify)",
-	"XDK_EVENT_GENERIC (GenericEvent)",
-	"XDK_EVENT_LAST (LASTEvent)",
+typedef void (* XEventToStringFunc)(XEvent * event, gchar * buf, gsize buf_size);
+
+typedef struct _XEventInfo XEventInfo;
+
+struct _XEventInfo
+{
+	const char * name;
+	
+	XEventToStringFunc to_string_func;
+};
+
+static XEventToStringFunc xdk_util_event_get_to_string_func(XEvent * event);
+static void key_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void button_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void motion_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void crossing_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void focus_change_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void expose_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void graphics_expose_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void no_expose_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void visibility_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void create_window_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void destroy_window_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void unmap_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void map_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void map_request_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void reparent_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void configure_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void gravity_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void resize_request_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void configure_request_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void circulate_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void circulate_request_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void property_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void selection_clear_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void selection_request_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void selection_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void colormap_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void client_message_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void mapping_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void generic_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void error_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+static void keymap_event_to_string(XEvent * event, gchar * buf, gsize buf_size);
+
+static char xdk_util_buf[1024];
+
+static const XEventInfo event_infos[] = {
+	{ "Unknown", NULL },
+	{ "Unknown", NULL },
+	{ "XDK_EVENT_KEY_PRESS (KeyPress)", key_event_to_string },
+	{ "XDK_EVENT_KEY_RELEASE (KeyRelease)", key_event_to_string },
+	{ "XDK_EVENT_BUTTON_PRESS (ButtonPress)", button_event_to_string },
+	{ "XDK_EVENT_BUTTON_RELEASE (ButtonRelease)", button_event_to_string },
+	{ "XDK_EVENT_MOTION (MotionNotify)", motion_event_to_string },
+	{ "XDK_EVENT_ENTER (EnterNotify)", crossing_event_to_string },
+	{ "XDK_EVENT_LEAVE (LeaveNotify)", crossing_event_to_string },
+	{ "XDK_EVENT_FOCUS_IN (FocusIn)", focus_change_event_to_string },
+	{ "XDK_EVENT_FOCUS_OUT (FocusOut)", focus_change_event_to_string },
+	{ "XDK_EVENT_KEYMAP (KeymapNotify)", keymap_event_to_string },
+	{ "XDK_EVENT_EXPOSE (Expose)", expose_event_to_string },
+	{ "XDK_EVENT_GRAPHICS_EXPOSE (GraphicsExpose)", graphics_expose_event_to_string },
+	{ "XDK_EVENT_NO_EXPOSE (NoExpose)", no_expose_event_to_string },
+	{ "XDK_EVENT_VISIBILITY (VisibilityNotify)", visibility_event_to_string },
+	{ "XDK_EVENT_CREATE (CreateNotify)", create_window_event_to_string },
+	{ "XDK_EVENT_DESTROY (DestroyNotify)", destroy_window_event_to_string },
+	{ "XDK_EVENT_UNMAP (UnmapNotify)", unmap_event_to_string },
+	{ "XDK_EVENT_MAP (MapNotify)", map_event_to_string },
+	{ "XDK_EVENT_MAP_REQUEST (MapRequest)", map_request_event_to_string },
+	{ "XDK_EVENT_REPARENT (ReparentNotify)", reparent_event_to_string },
+	{ "XDK_EVENT_CONFIGURE (ConfigureNotify)", configure_event_to_string },
+	{ "XDK_EVENT_CONFIGURE_REQUEST (ConfigureRequest)", configure_request_event_to_string },
+	{ "XDK_EVENT_GRAVITY (GravityNotify)", gravity_event_to_string },
+	{ "XDK_EVENT_RESIZE_REQUEST (ResizeRequest)", resize_request_event_to_string },
+	{ "XDK_EVENT_CIRCULATE (CirculateNotify)", circulate_event_to_string },
+	{ "XDK_EVENT_CIRCULATE_REQUEST = CirculateRequest)", circulate_request_event_to_string },
+	{ "XDK_EVENT_PROPERTY (PropertyNotify)", property_event_to_string },
+	{ "XDK_EVENT_SELECTION_CLEAR (SelectionClear)", selection_clear_event_to_string },
+	{ "XDK_EVENT_SELECTION_REQUEST (SelectionRequest)", selection_request_event_to_string },
+	{ "XDK_EVENT_SELECTION (SelectionNotify)", selection_event_to_string },
+	{ "XDK_EVENT_COLORMAP (ColormapNotify)", colormap_event_to_string },
+	{ "XDK_EVENT_CLIENT_MESSAGE (ClientMessage)", client_message_event_to_string },
+	{ "XDK_EVENT_MAPPING (MappingNotify)", mapping_event_to_string },
+	{ "XDK_EVENT_GENERIC (GenericEvent)", generic_event_to_string },
 };
 
 void xdk_util_event_dump(XEvent * event)
 {
-	gchar * desc = xdk_util_event_to_string(event);
-	if(desc) {
-		g_printerr("%s\n", desc);
-		g_free(desc);
-	}
+	g_printerr("%s\n", xdk_util_event_to_string(event));
 }
 
 gchar * xdk_util_event_to_string(XEvent * event)
 {
 	g_return_val_if_fail(event, NULL);
 	
-	return g_strdup_printf("%s:\n\tserial=%lu\n\tsend_event=%s\n\tdisplay=%s\n\twindow=%d",
+	gint len = g_snprintf(
+		xdk_util_buf, sizeof(xdk_util_buf),
+		"%s:\n\tserial=%lu\n\tsend_event=%s\n\tdisplay=%s\n\tevent=%lu",
 		xdk_util_event_get_name(event),
 		event->xany.serial,
 		event->xany.send_event ? "true" : "false",
 		XDisplayString(event->xany.display),
 		event->xany.window);
+		
+	XEventToStringFunc to_string_func = xdk_util_event_get_to_string_func(event);
+	if(to_string_func) {
+		to_string_func(event, xdk_util_buf + len, sizeof(xdk_util_buf) - len);
+	}
+	
+	return xdk_util_buf;
 }
 
 const char * xdk_util_event_get_name(XEvent * event)
 {
-	g_return_val_if_fail(event, event_names[0]);
-	g_return_val_if_fail(event->type < 2 || event->type >= G_N_ELEMENTS(* event_names), event_names[0]);
+	g_return_val_if_fail(event, "Unknown");
+	g_return_val_if_fail(event->type >= 2 && event->type < G_N_ELEMENTS(event_infos), "Unknown");
 	
-	return event_names[event->type];
+	return event_infos[event->type].name;
+}
+
+static XEventToStringFunc xdk_util_event_get_to_string_func(XEvent * event)
+{
+	g_return_val_if_fail(event, NULL);
+	g_return_val_if_fail(event->type >= 2 && event->type < G_N_ELEMENTS(event_infos), NULL);
+	
+	return event_infos[event->type].to_string_func;
+}
+
+static void key_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void button_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void motion_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void crossing_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+
+static void focus_change_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+
+static void expose_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+
+static void graphics_expose_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+
+static void no_expose_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+
+static void visibility_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+
+static void create_window_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+
+static void destroy_window_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+
+static void unmap_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void map_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	XMapEvent * e = (XMapEvent *) event;
+	g_snprintf(buf, buf_size,
+		"\n\twindow=%lu\n\toverride_redirect=%s",
+		e->window,
+		e->override_redirect ? "true" : "false");
+}
+
+static void map_request_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void reparent_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	XReparentEvent * e = (XReparentEvent *) event;
+	g_snprintf(buf, buf_size,
+		"\n\twindow=%lu\n\tparent=%lu\n\tx=%d, y=%d\n\toverride_redirect=%s",
+		e->window,
+		e->parent,
+		e->x, e->y,
+		e->override_redirect ? "true" : "false");
+}
+
+static void configure_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	XConfigureEvent * e = (XConfigureEvent *) event;
+	g_snprintf(buf, buf_size,
+		"\n\twindow=%lu\n\tx=%d, y=%d\n\twidth=%d, height=%d\n\tborder_width=%d\n\tabove(Window)=%lu\n\toverride_redirect=%s",
+		e->window,
+		e->x, e->y,
+		e->width, e->height,
+		e->border_width,
+		e->above,
+		e->override_redirect ? "true" : "false");
+}
+
+static void gravity_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void resize_request_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void configure_request_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void circulate_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void circulate_request_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void property_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void selection_clear_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void selection_request_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void selection_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void colormap_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void client_message_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void mapping_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void generic_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void error_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
+}
+
+static void keymap_event_to_string(XEvent * event, gchar * buf, gsize buf_size)
+{
+	
 }
