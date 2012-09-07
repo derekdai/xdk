052bcfd513ea367e4194f28c5534958973f75ac2
Derek Dai <derek@alright.com.tw>
2012-09-08 06:47:11 +0800

    Now, we can handle events from root which are redirected from other X clients

--- examples/xdk-root-struct-redirect.c
+++ examples/xdk-root-struct-redirect.c
@@ -1,5 +1,7 @@
 #include <xdk/xdk.h>
 
+XdkWindow * root;
+
 XdkWindow * get_window(Window xid)
 {
 	XdkDisplay * display = xdk_display_get_default();
@@ -7,48 +9,66 @@ XdkWindow * get_window(Window xid)
 	if(! win) {
 		win = xdk_window_new();
 		xdk_window_set_foreign_peer(win, xid);
-		xdk_window_select_input(win, XDK_EVENT_MASK_STRUCTURE_NOTIFY);
+		xdk_window_select_input(win, XDK_EVENT_MASK_STRUCTURE_NOTIFY | XDK_EVENT_MASK_STRUCTURE_NOTIFY);
 	}
 	
 	return win;
 }
 
-gboolean on_event(XdkWindow * window, XEvent * event)
+gboolean on_event(XdkDisplay * display, XEvent * event, XdkWindow * root)
 {
-	gboolean result = FALSE;
-	
-	XdkDisplay * display = xdk_display_get_default();
-	
 	switch(event->type) {
-	case XDK_EVENT_CONFIGURE_REQUEST: {
-		XConfigureRequestEvent * e = (XConfigureRequestEvent *) event;
-		XWindowChanges change = {
-			.x = e->x,
-			.y = e->y,
-			.width = e->width,
-			.height = e->height,
-			.border_width = e->border_width,
-			.sibling = e->above,
-			.stack_mode = e->detail
-		};
-		XConfigureWindow(
-			xdk_display_get_peer(display),
-			e->window,
-			e->value_mask,
-			& change);
-		result = TRUE;
-		break;
-		}
-	case XDK_EVENT_MAP_REQUEST: {
-		XMapRequestEvent * e = (XMapRequestEvent *) event;
-		XdkWindow * win = get_window(e->window);
-		xdk_window_map(win);
-		result = TRUE;
+	case XDK_EVENT_CONFIGURE_REQUEST:
+	case XDK_EVENT_MAP_REQUEST:
+	case XDK_EVENT_RESIZE_REQUEST:
+	case XDK_EVENT_CIRCULATE_REQUEST:
+	case XDK_EVENT_DESTROY: {
+		event->xany.window = xdk_window_get_peer(root);
 		break;
 		}
 	}
 	
-	return result;
+	return FALSE;
+}
+
+void on_configure_request(XdkWindow * root, XEvent * event, XdkDisplay * display)
+{
+	XConfigureRequestEvent * e = (XConfigureRequestEvent *) event;
+	XWindowChanges change = {
+		.x = e->x,
+		.y = e->y,
+		.width = e->width,
+		.height = e->height,
+		.border_width = e->border_width,
+		.sibling = e->above,
+		.stack_mode = e->detail
+	};
+	XConfigureWindow(
+		xdk_display_get_peer(display),
+		e->window,
+		e->value_mask,
+		& change);
+}
+
+void on_map_request(XdkWindow * root, XEvent * event, XdkDisplay * display)
+{
+	XMapRequestEvent * e = (XMapRequestEvent *) event;
+	XdkWindow * win = get_window(e->window);
+	xdk_window_map(win);
+}
+
+void on_resize_request(XdkWindow * root, XEvent * event, XdkDisplay * display)
+{
+}
+
+void on_destroy_notify(XdkWindow * root, XEvent * event, XdkDisplay * display)
+{
+	XDestroyWindowEvent * e = (XDestroyWindowEvent *) event;
+	XdkWindow * win = xdk_display_lookup_window(display, e->window);
+	if(win) {
+		xdk_window_destroy(win);
+		g_object_unref(win);
+	}
 }
 
 int main()
@@ -56,8 +76,9 @@ int main()
 	xdk_init(NULL, NULL);
 	
 	XdkDisplay * display = xdk_display_get_default();
-	
 	XdkWindow * root = xdk_get_default_root_window();
+
+	xdk_display_add_event_filter(display, (XdkEventFilter *) on_event, root);
 	
 	xdk_trap_error();
 	xdk_window_select_input(root, XDK_EVENT_MASK_SUBSTRUCTURE_REDIRECT);
@@ -68,7 +89,10 @@ int main()
 		g_error("Maybe another window manager is running? %s", error->message);
 	}
 	
-	g_signal_connect(root, "event-filter", G_CALLBACK(on_event), NULL);
+	g_signal_connect(root, "configure-request", G_CALLBACK(on_configure_request), display);
+	g_signal_connect(root, "map-request", G_CALLBACK(on_map_request), display);
+	g_signal_connect(root, "resize-request", G_CALLBACK(on_resize_request), display);
+	g_signal_connect(root, "destroy-notify", G_CALLBACK(on_destroy_notify), display);
 	
 	xdk_main();
 
