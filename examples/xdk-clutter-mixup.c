844ed4cc279a2b9bdafaee8f5bffb4d9eae649af
Derek Dai <derek@alright.com.tw>
2012-09-11 08:36:09 +0800

    examples/xdk-clutter-mixup.c: let clutter as the main event handler and install Xdk's own event filter onto it

--- examples/xdk-clutter-mixup.c
+++ examples/xdk-clutter-mixup.c
@@ -2,31 +2,37 @@
 #include <clutter/clutter.h>
 #include <clutter/x11/clutter-x11.h>
 
-gboolean on_event(XdkDisplay * display, XEvent * event)
+ClutterX11FilterReturn on_event(
+	XEvent * xev,
+	ClutterEvent * cev,
+	XdkDisplay * display)
 {
-	if(CLUTTER_X11_FILTER_CONTINUE == clutter_x11_handle_event(event)) {
-		return FALSE;
+	if(xdk_display_handle_event(display, xev)) {
+		CLUTTER_X11_FILTER_REMOVE;
 	}
 	
-	return TRUE;
+	return CLUTTER_X11_FILTER_CONTINUE;
 }
 
 int main(int argc, char * args[])
 {
-	xdk_init(& argc, & args);
-	XdkDisplay * display = xdk_display_get_default();
-	clutter_x11_disable_event_retrieval();
-	clutter_x11_set_display(xdk_display_get_peer(display));
 	clutter_init(& argc, & args);
-	
-	xdk_display_add_event_filter(display, (XdkEventFilter) on_event, NULL);
-	
+
+	xdk_disable_event_retrieval();
+	xdk_set_default_xdisplay(clutter_x11_get_default_display());
+	xdk_init(& argc, & args);
+	clutter_x11_add_filter(
+		(ClutterX11FilterFunc) on_event,
+		xdk_display_get_default());
+
 	ClutterActor * stage = clutter_stage_new();
-	g_signal_connect(stage, "destroy", G_CALLBACK(xdk_main_quit), NULL);
+	clutter_actor_set_reactive(stage, TRUE);
+	g_signal_connect(stage, "destroy", G_CALLBACK(clutter_main_quit), NULL);
+	clutter_stage_set_user_resizable(CLUTTER_STAGE(stage), TRUE);
+	clutter_actor_set_background_color(stage, clutter_color_get_static(CLUTTER_COLOR_TRANSPARENT));
 	clutter_actor_show(stage);
 	
-	xdk_main();
+	clutter_main();
 	
 	return 0;
 }
-
